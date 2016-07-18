10669041ac98b1098af3fc2d0804f4c1872f05ee
Brent Perry <burentoperii@gmail.com>
2016-07-18 14:49:42 -0500

    Add game_stats program

--- Ch1/game_stats.cpp
+++ Ch1/game_stats.cpp
@@ -5,37 +5,35 @@ using namespace std;
 
 int main()
 {
-    int score;
-    double distance;
-    char playAgain;
-    bool shieldsUp;
+    unsigned int score = 5000;
+    cout << "score: " << score << endl;
 
-    short lives, aliensKilled;
+    score = score + 100;
+    cout << "score: " << score << endl;
 
-    score = 0;
-    distance = 1200.76;
-    playAgain = 'y';
-    shieldsUp = true;
-    lives = 3;
-    aliensKilled = 10;
+    score += 100;
+    cout << "score: " << score << endl;
 
-    double engineTemp = 6572.89;
-
-    cout << "\nscore: " << score << endl;
-    cout << "distance: " << distance << endl;
-    cout << "playAgain: " << playAgain << endl;
+    int lives = 3;
+    ++lives;
     cout << "lives: " << lives << endl;
-    cout << "aliensKilled: " << aliensKilled << endl;
-    cout << "engineTemp: " << engineTemp << endl;
 
-    int fuel;
-    cout << "\nHow much fuel? ";
-    cin >> fuel;
-    cout << "fuel: " << fuel << endl;
+    lives = 3;
+    lives++;
+    cout << "lives: " << lives << endl;
 
-    typedef unsigned short int ushort;
-    ushort bonus = 10;
-    cout << "\nbonus: " << bonus << endl;
+    lives = 3;
+    int bonus = ++lives * 10;
+    cout << "lives, bonus = " << lives << ", " << bonus << endl;
+
+    lives = 3;
+    bonus = lives++ * 10;
+    cout << "lives, bonus = " << lives << ", " << bonus << endl;
+
+    score = 4294967295;
+    cout << "\nscore: " << score << endl;
+    ++score;
+    cout << "score: " << score << endl;
 
     return 0;
 }
