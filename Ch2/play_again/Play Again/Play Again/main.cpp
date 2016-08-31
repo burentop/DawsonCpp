59256501b33d125fb9231991c570bede7caed3f8
Brent Perry <burentoperii@gmail.com>
2016-08-31 17:05:34 -0500

    Change to do/while loop

--- Ch2/play_again/Play Again/Play Again/main.cpp
+++ Ch2/play_again/Play Again/Play Again/main.cpp
@@ -11,14 +11,14 @@ using namespace std;
 
 int main() {
     
-    char again = 'y';
+    char again;
     
-    while (again == 'y')
+    do
     {
         cout << "\n** Played an exciting game **";
         cout << "\nDo you want to play again? (y/n): ";
         cin >> again;
-    }
+    } while (again == 'y');
     
     cout << "\nOkay, bye.";
     
