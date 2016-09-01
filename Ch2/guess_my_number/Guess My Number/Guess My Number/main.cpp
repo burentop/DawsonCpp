54bd726a4443b6108bc61e61eba588e1593cf29a
Brent Perry <burentoperii@gmail.com>
2016-09-01 09:20:57 -0500

    Reverse roles (computer/guesser) in Guess My Number

--- Ch2/guess_my_number/Guess My Number/Guess My Number/main.cpp
+++ Ch2/guess_my_number/Guess My Number/Guess My Number/main.cpp
@@ -16,31 +16,34 @@ int main() {
     
     srand(static_cast<unsigned int>(time(0))); // seed random number
     
-    int secretNumber = rand() % 100 + 1;       // random number between 1 and 100
+    cout << "\tWelcome to Guess My Number\n\n";
+    
+    int userNumber;
+    cout << "Enter a number between 1 - 100 for the computer to guess: ";
+    cin >> userNumber;
+    
     int tries = 0;
     int guess;
     
-    cout << "\tWelcome to Guess My Number\n\n";
-    
     do
     {
-        cout << "Enter a guess: ";
-        cin >> guess;
+        guess = rand() % 100 + 1;       // random number between 1 and 100
         ++tries;
+        cout << "The computer guesses " << guess << ".\n";
         
-        if (guess > secretNumber)
+        if (guess > userNumber)
         {
             cout << "Too high!\n\n";
         }
-        else if (guess < secretNumber)
+        else if (guess < userNumber)
         {
             cout << "Too low!\n\n";
         }
         else
         {
-            cout << "\nThat's it! You got it in " << tries << " guesses!\n";
+            cout << "\nThat's it! The computer guessed it in " << tries << " guesses!\n";
         }
-    } while (guess != secretNumber);
+    } while (guess != userNumber);
     
     return 0;
 }
